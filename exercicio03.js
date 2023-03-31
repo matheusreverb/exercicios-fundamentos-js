f1f706c505fcddbb7313cb8cd2220a961bfdac9b
Matheus <devmatheus12@gmail.com>
2023-03-31 13:56:56 -0300

    Resolução do exercicio03.js

---
+++ exercicio03.js
@@ -0,0 +1,8 @@
+//Desenvolva uma função que recebe dois parâmetros, um é a quantidade de horas trabalhadas por um funcionário num mês, e o quanto ele recebe por hora.
+//O retorno da função deve ser a string "Salário igul a R$ X", em que X é o quanto o funcionário ganhou no mês.
+
+function calcularSalário(horasTrabalhadas, ValorDaHora) {
+    return console.log(horasTrabalhadas * ValorDaHora)
+}
+
+calcularSalário(150, 40.5)
