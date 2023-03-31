a5ea4543eccfa8ec913aa0949ac0c7bfb6f81779
Matheus <devmatheus12@gmail.com>
2023-03-31 17:03:52 -0300

    Resolução do exercicio06.js

--- exercicios06.js
+++ exercicios06.js
@@ -1,9 +1,8 @@
 //Escreva uma função que receba um valor booleano ou número.
 //Se o parâmetro fornecido for booleano, o retorno da função deverá ser o inverso.
 //Por exemplo, se a entrada for false, retornará true. Se o parâmetro for número,
-//o retorno será o número inverso. Por exemplo, se for fornecido1, o retorno será -1.
-//Se o parâmetro de entrada não for de nenhum dos tipos acima, retorne "booleano ou número esperados, mas o
-//parâmetro é do tipo ...".
+//o retorno será o número inverso. Por exemplo, se for fornecido 1, o retorno será -1.
+//Se o parâmetro de entrada não for de nenhum dos tipos acima, retorne "booleano ou número esperados, mas o parâmetro é do tipo ...".
 
 function inverso(args) {
     if (typeof args === "boolean") {
@@ -12,7 +11,7 @@ function inverso(args) {
     else if (typeof args === "number") {
         return console.log(args - args * 2)
     } else {
-        console.error("Essa função só aceita booleanos ou números, mas o parâmetro é do tipo String")
+        return console.error("Essa função só aceita booleanos ou números, mas o parâmetro é do tipo String")
     }
 
 }
