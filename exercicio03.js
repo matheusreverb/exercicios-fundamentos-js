f897f6821e01b50b32dcdf3d3b986c45bb7e7402
Matheus <devmatheus12@gmail.com>
2023-03-31 17:40:13 -0300

    Resolução do exercicio07.js

--- exercicio03.js
+++ exercicio03.js
@@ -1,5 +1,5 @@
 //Desenvolva uma função que recebe dois parâmetros, um é a quantidade de horas trabalhadas por um funcionário num mês, e o quanto ele recebe por hora.
-//O retorno da função deve ser a string "Salário igul a R$ X", em que X é o quanto o funcionário ganhou no mês.
+//O retorno da função deve ser a string "Salário igual a R$ X", em que X é o quanto o funcionário ganhou no mês.
 
 function calcularSalário(horasTrabalhadas, ValorDaHora) {
     return console.log(horasTrabalhadas * ValorDaHora)
---
+++ exercicio07.js
@@ -0,0 +1,20 @@
+//Crie uma função que receba quatro números como parâmetros(numero, minimo, maximo, inclusivo) e retorne se o parâmetro numero (o primeiro) está entre minimo e maximo.
+//Quando o parâmetro inclusivo for true, tenha "entre" como inclusivo, ou seja, considerando se numero é igual a minimo ou a maximo.
+//Caso o parâmetro inclusivo não seja informado, seu valor padrão deverá ser false, portanto, a lógica será exclusiva, não considerando se numero é igual a minimo ou a maximo.
+
+function inBetween(num, min, max, inc) {
+    if (inc == true) {
+        return getResult(num, min, max)
+    } else {
+        return console.log(false)
+    }
+}
+
+function getResult(num, min, max) {
+    console.log(num >= min && num <= max)
+}
+
+inBetween(101, 11, 100, true)
+
+//True
+//False
